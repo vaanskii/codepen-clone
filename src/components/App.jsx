0b102658890ab972a02f88b5627164e275d8d20f
vaanskii <giorgivanadze03@gmail.com>
2024-08-16 07:30:47 +0400

    Added coding functionality in project.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,25 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import Editor from "./Editor"
 
 function App() {
   const [html, setHtml] = useState('');
   const [css, setCss] = useState('');
   const [js, setJs] = useState('');
+  const [srcDoc, setSrcDoc] = useState('');
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+    <html>
+      <body>${html}</body>
+      <style>${css}</style>
+      <script>${js}</script>
+    </html>`
+  )
+    }, 250)
+
+    return () => clearTimeout(timeout)
+  }, [html, css, js])
 
   return (
     <>
@@ -15,6 +30,7 @@ function App() {
       </div>
       <div className="pane ">
         <iframe 
+          srcDoc={srcDoc}
           style={{ border: "none" }}
           title="output" 
           sandbox="allow-scripts"
