dedcbb06a2a63e7e765a8b72bc13a6e81430361e
vaanskii <giorgivanadze03@gmail.com>
2024-08-16 07:43:20 +0400

    Merge css files. - Merge css files and added more styles for editor. - Implemented button onclick function to change sizes each (html,css,js) editor.

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/material.css';
 import 'codemirror/mode/xml/xml';
@@ -15,15 +15,17 @@ const Editor = (props) => {
         onChange
     } = props
 
+    const [open, setOpen] = useState(true)
+
     function handleChange(editor, data, value) {
         onChange(value)
     }
 
     return (
-        <div className='editor-container'>
+        <div className={`editor-container ${open ? '' : 'collapsed'}`}>
             <div className="editor-title">
                 {displayName}
-                <button>0/C</button>
+                <button onClick={() => setOpen(prevOpen => !prevOpen)}>0/C</button>
             </div>  
             <ControlledEditor 
                 onBeforeChange={handleChange}
